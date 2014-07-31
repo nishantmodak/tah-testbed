a9c4362950bc6682df060e9915ffa7521d64cb06
aliasgermodi <modialiasger@gmail.com>
2014-07-31 15:53:09 +0530

    Function to initislise all the gpio pins used

---
+++ func.py
@@ -0,0 +1,19 @@
+import RPi.GPIO as GPIO
+
+def init():
+	
+	# to use Raspberry Pi board pin numbers 
+	GPIO.setmode(GPIO.BOARD)
+	# set up GPIO output channel 	 
+	GPIO.setup(3, GPIO.OUT)
+	GPIO.setup(5, GPIO.OUT)
+	GPIO.setup(10, GPIO.OUT)
+	GPIO.setup(8, GPIO.OUT)	
+	GPIO.setup(7, GPIO.OUT)
+	GPIO.setup(3, GPIO.LOW)
+        GPIO.setup(5, GPIO.LOW)
+        GPIO.setup(10, GPIO.LOW)
+        GPIO.setup(8, GPIO.LOW) 
+        GPIO.setup(7, GPIO.LOW)
+
+
