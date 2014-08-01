784bdca2e7b0034343d12c4a9c3654fe921e1d07
aliasgermodi <modialiasger@gmail.com>
2014-08-01 18:59:00 +0530

    Added the physical fault button.

--- run.py
+++ run.py
@@ -6,27 +6,36 @@ import os
 #buttonPin = 11
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(11,GPIO.IN)
-GPIO.setup(15,GPIO.OUT)
-#input = GPIO.input(17)
-
-#while True:
-  #assuming the script to call is long enough we can ignore bouncing
- # if (GPIO.input(17)):
-    #this is the script that will be called (as root)
-    #os.system("sudo python /home/pi/led_on.py")
+GPIO.setup(23,GPIO.OUT)
+GPIO.setup(13,GPIO.IN)
 prev_input = 0
 while True:
   #take a reading
   input = GPIO.input(11)
+  input1 = GPIO.input(13)	
   #if the last reading was low and this one high, print
   if ((not prev_input) and input):
-    print("START")	
+    GPIO.output(23,GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(23,GPIO.LOW)
     #once the button is pressed, the script will run and there will be a single beep sound.
-    os.system('python /home/pi/tah_final.py')
-    GPIO.output(15,GPIO.HIGH)
-    time.sleep(0.5)
-    GPIO.output(15,GPIO.LOW)		 
-  #update previous input
+    os.system('python /home/pi/Ali/tah-testbed/lcd_rpi.py')
+    os.system('python /home/pi/Ali/tah-testbed/tah_final.py')
+    #update previous input
   prev_input = input
   #slight pause to debounce
   time.sleep(0.05)
+  
+  if ((not prev_input) and input1):
+    GPIO.output(23,GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(23,GPIO.LOW)
+    time.sleep(1)
+    GPIO.output(23,GPIO.HIGH)
+    time.sleep(1)
+    GPIO.output(23,GPIO.LOW)
+  prev_input = input
+  #slight pause to debounce
+  time.sleep(0.05)		
+    #once the button is pressed, the script will run and there will be a single beep sound.
+
