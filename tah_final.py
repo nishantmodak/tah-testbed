59928a3d16d7d2028c921fe425a02c512afdefdf
aliasgermodi <modialiasger@gmail.com>
2014-08-10 17:50:50 +0530

    Decreased the time delay, Removed OK and FAIL functions

--- tah_final.py
+++ tah_final.py
@@ -2,8 +2,6 @@ import RPi.GPIO as GPIO
 import time
 import func
 import lcd_rpi
-import lcd_ok
-import lcd_fail
 import serial
 import lcd_over
 
@@ -18,84 +16,84 @@ GPIO.output(3,GPIO.LOW)
 GPIO.output(5,GPIO.LOW)
 GPIO.output(8,GPIO.LOW)
 GPIO.output(10,GPIO.LOW)
-time.sleep(2)
+time.sleep(1)
 
 #I/O=12
 GPIO.output(3,GPIO.HIGH)	
 GPIO.output(5,GPIO.LOW)
 GPIO.output(8,GPIO.LOW)
 GPIO.output(10,GPIO.LOW)
-time.sleep(2)
+time.sleep(1)
 
 #I/0=11
 GPIO.output(3,GPIO.LOW)
 GPIO.output(5,GPIO.HIGH)
 GPIO.output(8,GPIO.LOW)
 GPIO.output(10,GPIO.LOW)
-time.sleep(2)
+time.sleep(1)
 
 #I/O=10
 GPIO.output(3,GPIO.HIGH)
 GPIO.output(5,GPIO.HIGH)
 GPIO.output(8,GPIO.LOW)
 GPIO.output(10,GPIO.LOW)
-time.sleep(2)
+time.sleep(1)
 
 #I/0=9
 GPIO.output(3,GPIO.LOW)
 GPIO.output(5,GPIO.LOW)
 GPIO.output(8,GPIO.HIGH)
 GPIO.output(10,GPIO.LOW)
-time.sleep(2)
+time.sleep(1)
 	
 #I/0=8
 GPIO.output(3,GPIO.HIGH)
 GPIO.output(5,GPIO.LOW)
 GPIO.output(8,GPIO.HIGH)
 GPIO.output(10,GPIO.LOW)
-time.sleep(2)
+time.sleep(1)
 
 #I/O=7
 GPIO.output(3,GPIO.LOW)
 GPIO.output(5,GPIO.HIGH)
 GPIO.output(8,GPIO.HIGH)
 GPIO.output(10,GPIO.LOW)
-time.sleep(2)
+time.sleep(1)
 	
 #I/O=6
 GPIO.output(3,GPIO.HIGH)
 GPIO.output(5,GPIO.HIGH)
 GPIO.output(8,GPIO.HIGH)
 GPIO.output(10,GPIO.LOW)
-time.sleep(2)
+time.sleep(1)
 	
 #I/0=5
 GPIO.output(3,GPIO.LOW)
 GPIO.output(5,GPIO.LOW)
 GPIO.output(8,GPIO.LOW)
 GPIO.output(10,GPIO.HIGH)
-time.sleep(2)
+time.sleep(1)
 	
 #I/0=4
 GPIO.output(3,GPIO.HIGH)
 GPIO.output(5,GPIO.LOW)
 GPIO.output(8,GPIO.LOW)
 GPIO.output(10,GPIO.HIGH)
-time.sleep(2)
+time.sleep(1)
 		
 #I/0=3
 GPIO.output(3,GPIO.LOW)
 GPIO.output(5,GPIO.HIGH)
 GPIO.output(8,GPIO.LOW)
 GPIO.output(10,GPIO.HIGH)
-time.sleep(2)
+time.sleep(1)
 	
 #I/0=2
 GPIO.output(3,GPIO.HIGH)
 GPIO.output(5,GPIO.HIGH)
 GPIO.output(8,GPIO.LOW)
 GPIO.output(10,GPIO.HIGH)
-time.sleep(2)
+time.sleep(1)
 	
 
 GPIO.output(7,GPIO.LOW)
@@ -105,84 +103,84 @@ GPIO.output(3,GPIO.LOW)
 GPIO.output(5,GPIO.LOW)
 GPIO.output(8,GPIO.LOW)
 GPIO.output(10,GPIO.LOW)
-time.sleep(2)
+time.sleep(1)
 
 #I/O=12
 GPIO.output(3,GPIO.HIGH)
 GPIO.output(5,GPIO.LOW)
 GPIO.output(8,GPIO.LOW)
 GPIO.output(10,GPIO.LOW)
-time.sleep(2)
+time.sleep(1)
      
 #I/0=11
 GPIO.output(3,GPIO.LOW)
 GPIO.output(5,GPIO.HIGH)
 GPIO.output(8,GPIO.LOW)
 GPIO.output(10,GPIO.LOW)
-time.sleep(2)
+time.sleep(1)
 
 #I/O=10
 GPIO.output(3,GPIO.HIGH)
 GPIO.output(5,GPIO.HIGH)
 GPIO.output(8,GPIO.LOW)
 GPIO.output(10,GPIO.LOW)
-time.sleep(2)
+time.sleep(1)
 
 #I/0=9
 GPIO.output(3,GPIO.LOW)
 GPIO.output(5,GPIO.LOW)
 GPIO.output(8,GPIO.HIGH)
 GPIO.output(10,GPIO.LOW)
-time.sleep(2)
+time.sleep(1)
 
 #I/0=8
 GPIO.output(3,GPIO.HIGH)
 GPIO.output(5,GPIO.LOW)
 GPIO.output(8,GPIO.HIGH)
 GPIO.output(10,GPIO.LOW)
-time.sleep(2)
+time.sleep(1)
 
 #I/O=7
 GPIO.output(3,GPIO.LOW)
 GPIO.output(5,GPIO.HIGH)
 GPIO.output(8,GPIO.HIGH)
 GPIO.output(10,GPIO.LOW)
-time.sleep(2)
+time.sleep(1)
 
 #I/O=6
 GPIO.output(3,GPIO.HIGH)
 GPIO.output(5,GPIO.HIGH)
 GPIO.output(8,GPIO.HIGH)
 GPIO.output(10,GPIO.LOW)
-time.sleep(2)
+time.sleep(1)
 
 #I/0=5
 GPIO.output(3,GPIO.LOW)
 GPIO.output(5,GPIO.LOW)
 GPIO.output(8,GPIO.LOW)
 GPIO.output(10,GPIO.HIGH)
-time.sleep(2)
+time.sleep(1)
 
 #I/0=4
 GPIO.output(3,GPIO.HIGH)
 GPIO.output(5,GPIO.LOW)
 GPIO.output(8,GPIO.LOW)
 GPIO.output(10,GPIO.HIGH)
-time.sleep(2)
+time.sleep(1)
 
 #I/0=3
 GPIO.output(3,GPIO.LOW)
 GPIO.output(5,GPIO.HIGH)
 GPIO.output(8,GPIO.LOW)
 GPIO.output(10,GPIO.HIGH)
-time.sleep(2)
+time.sleep(1)
 
 #I/0=2
 GPIO.output(3,GPIO.HIGH)
 GPIO.output(5,GPIO.HIGH)
 GPIO.output(8,GPIO.LOW)
 GPIO.output(10,GPIO.HIGH)
-time.sleep(2)
+time.sleep(1)
 
 #We start testing the analog I/O, i.e from pin A0-A5.
 
@@ -192,37 +190,37 @@ GPIO.output(7,GPIO.HIGH)
 GPIO.output(3,GPIO.HIGH)
 GPIO.output(5,GPIO.HIGH)
 GPIO.output(8,GPIO.HIGH)	
-time.sleep(2)
+time.sleep(1)
 
 #A1
 GPIO.output(3,GPIO.HIGH)	
 GPIO.output(5,GPIO.LOW)
 GPIO.output(8,GPIO.HIGH)
-time.sleep(2)
+time.sleep(1)
 
 #A2
 GPIO.output(3,GPIO.LOW)
 GPIO.output(5,GPIO.HIGH)
 GPIO.output(8,GPIO.HIGH)
-time.sleep(2)
+time.sleep(1)
 
 #A3
 GPIO.output(3,GPIO.LOW)
 GPIO.output(5,GPIO.LOW)
 GPIO.output(8,GPIO.HIGH)
-time.sleep(2)
+time.sleep(1)
 	
 #A4
 GPIO.output(3,GPIO.LOW)
 GPIO.output(5,GPIO.HIGH)
 GPIO.output(8,GPIO.LOW)
-time.sleep(2)
+time.sleep(1)
 	
 #A5
 GPIO.output(3,GPIO.HIGH)
 GPIO.output(5,GPIO.LOW)
 GPIO.output(8,GPIO.LOW)
-time.sleep(2)
+time.sleep(1)
 
 GPIO.output(7,GPIO.LOW)
 
@@ -231,46 +229,46 @@ GPIO.output(7,GPIO.LOW)
 GPIO.output(3,GPIO.HIGH)
 GPIO.output(5,GPIO.HIGH)
 GPIO.output(8,GPIO.HIGH)	
-time.sleep(2)
+time.sleep(1)
 
 #A1
 GPIO.output(3,GPIO.HIGH)	
 GPIO.output(5,GPIO.LOW)
 GPIO.output(8,GPIO.HIGH)
-time.sleep(2)
+time.sleep(1)
 
 #A2
 GPIO.output(3,GPIO.LOW)
 GPIO.output(5,GPIO.HIGH)
 GPIO.output(8,GPIO.HIGH)
-time.sleep(2)
+time.sleep(1)
 
 #A3
 GPIO.output(3,GPIO.LOW)
 GPIO.output(5,GPIO.LOW)
 GPIO.output(8,GPIO.HIGH)
-time.sleep(2)
+time.sleep(1)
 
 #A4
 GPIO.output(3,GPIO.LOW)
 GPIO.output(5,GPIO.HIGH)
 GPIO.output(8,GPIO.LOW)
-time.sleep(2)
+time.sleep(1)
 	
 #A5
 GPIO.output(3,GPIO.HIGH)
 GPIO.output(5,GPIO.LOW)
 GPIO.output(8,GPIO.LOW)
-time.sleep(2)
+time.sleep(1)
 
 ser = serial.Serial('/dev/ttyACM0', 9600)
 if ser == "ok":
-        lcd_ok.main()
+        lcd_rpi.main()
         GPIO.output(23,GPIO.HIGH)
         time.sleep(1)
         GPIO.output(23,GPIO.LOW)
 elif ser == "fail":
-        lcd_fail.main()
+        lcd_rpi.main()
         GPIO.output(23,GPIO.HIGH)
         time.sleep(1)
         GPIO.output(23,GPIO.LOW)
